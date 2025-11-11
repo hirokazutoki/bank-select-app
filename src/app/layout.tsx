ff257ecf638e7822e4cf820bf258fdd9db1e0f26
hirokazutoki <hrkztk1124@gmail.com>
2025-11-11 22:50:07 +0900

    add: Make the app title changeable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "ebank Client Demo",
+  title: process.env.APP_NAME ?? "BANK SELECT APP",
   description: "",
 };
 
