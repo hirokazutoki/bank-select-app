9ed50da4bfa173f08ea7d1061c1a911ea7c6d866
hirokazutoki <hrkztk1124@gmail.com>
2026-01-07 23:31:34 +0900

    add: Prevent the login screen from being displayed while logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,15 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     // Cookie からトークンを取得
     const token = request.cookies.get('session')?.value
+    const { pathname } = request.nextUrl;
+
+    // トークンを保持していて、 /login に来たら / にリダイレクト
+    if (token && pathname === "/login") {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
 
     // トークンがなければ /login にリダイレクト
-    if (!token && request.nextUrl.pathname !== '/login') {
+    if (!token && pathname !== '/login') {
         const loginUrl = new URL('/login', request.url)
         return NextResponse.redirect(loginUrl)
     }
@@ -16,5 +22,5 @@ export function middleware(request: NextRequest) {
 
 // どのルートでこのmiddlewareを有効にするか
 export const config = {
-    matcher: ['/'],
+    matcher: ["/((?!api|_next|favicon.ico).*)"],
 }
